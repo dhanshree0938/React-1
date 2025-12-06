13f811d0f5f5bf173ec40f97176d29d8a2b38414
Dhanshree Mali <dmali9798@gmail.com>
2025-12-06 21:03:42 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,14 +1,44 @@
-import React from 'react'
-import Welcome from './Welcome';
+// import logo from './logo.svg';
+// import './App.css';
+import { useState } from "react";
+import Button from "./Button";
+import Display from "./Display";
 
-export default function App() {
+// function App() {
+//   return (
+//     // <h2>App component</h2>
+//     <>
+//       <h2>App component</h2>
+//       <Button />
+//       <Display username="Dhanshree" age="20" />
+//       {/* We are sending JSON object to the Display component
+//           props= {
+//                     username : "Dhanshree";
+//                     "age" : "20";
+//                  }
+//          */}
+//          <Display username="Rohit"/>
+//          <Display username="Yassh" dob="25 jan 2020"/>
+//     </>
+//   );
+// }
+
+
+// App component will maintain the count state
+function App(){
+
+  const [count, setCount] = useState(0);
+  const updateCount = ()=> {
+    setCount(count+1);
+  }
   return (
     <>
-     <h1>App component -root component</h1>
-     <Welcome />
-     {/*Rendering the Mutliple items*/}
-     <Welcome />
-     <Welcome />
+    <h2>App component managing the count state</h2>
+    <Button onIncrement={updateCount}/>
+    <Display countVal={count}/>
+    {/*JSON
+     props = {countval : count}*/}
     </>
-  );
+  )
 }
+export default App;
--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import './index.css';
 import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-// creating the components separately
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-// index.js is the first js file, that gets excuted when the app is started 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
 
-// App.js -  root componenet
-const div = document.getElementById('root');
-const root = ReactDOM.createRoot(div);
-root.render(<App />);
+// Understanding virtual and Real DOM
+//RealDOM
+// const root1 = document.getElementById("root1"); //RealDOM
+// const root2 = ReactDOM.createRoot(document.getElementById('root2'));
+
+// setInterval(()=>{
+//   root1.innerHTML = `
+//    <h2>Real DOM</h2>
+//    <pre>${new Date().toTimeString()}</pre>
+//   `;
+
+//   root2.render(
+//     <>
+//     <h2>Virtual DOM</h2>
+//     <pre>{new Date().toTimeString()}</pre>
+//     </>
+//   )
+// },1000);
